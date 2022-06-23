e63880c22f6ab4cacee4af9baef8a2f3cd28a6d8
muuza <muuzagit@gmail.com>
2022-06-24 00:26:31 +0500

    change flutter name

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('FIRST FLUTTER APP'),
+        child: Text("flutter app"),
       ),
     );
   }
